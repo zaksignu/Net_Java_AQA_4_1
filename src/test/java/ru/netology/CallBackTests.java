759c87a17f14fe0df1adc5465d6ce9091d821063
zaksignu <zaksignu@gmail.com>
2022-03-28 17:06:21 +0300

    Minor commit

--- src/test/java/ru/netology/CallBackTests.java
+++ src/test/java/ru/netology/CallBackTests.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -7,10 +8,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-
 public class CallBackTests {
     private WebDriver driver;
 
@@ -21,11 +21,12 @@ public class CallBackTests {
 
     @BeforeEach
     public void setUP() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
+//        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
     }
 
     @AfterEach
@@ -37,7 +38,24 @@ public class CallBackTests {
     @Test
     void shouldSubmit(){
         open("http://localhost:9999");
+        $("[data-test-id=\"city\"] .input__control").setValue("Петрозаводск");
+  //      $("[data-test-id=\"date\"] .input__control").click();
 
+        $("[data-test-id=\"date\"] .input__control").clear();
+        $("[data-test-id=\"date\"] .input__control").setValue("01.04.2022");
+  //      $("[data-test-id=\"date\"] .input__control").click();
+        $("[data-test-id=\"name\"] .input__control").setValue("Пупкин Василий");
+        $("[data-test-id=\"phone\"] .input__control").setValue("+79123456789");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+//        $(".form-field .button__content").click();
+//        $(".form-field .button__content").wait();
 
+        $("[data-test-id=\"notification\"] .notification__title").shouldHave(Condition.matchText("Успешно"));
+
+        //    $("[data-test-id=\"notification\"] .notification__title").shouldHave("")getText()
+     //   data-test-id="notification"
+  //      $("[data-test-id=\"city\"] .input__control").click();
+
+        System.out.println();
     }
 }
