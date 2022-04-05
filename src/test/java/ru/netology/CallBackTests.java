000e8163c9b824f9db132b91bbcd25b1fe8b913a
zaksignu <zaksignu@gmail.com>
2022-04-05 10:31:40 +0300

    App changing

--- src/test/java/ru/netology/CallBackTests.java
+++ src/test/java/ru/netology/CallBackTests.java
@@ -4,10 +4,10 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.text.SimpleDateFormat;
+
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -53,39 +53,39 @@ public class CallBackTests {
         $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 
-    @Test
-    public void shouldWorkDropDownCity() {
-        $("[data-test-id=\"city\"] .input__control").setValue("горо");
+//    @Test
+//    public void shouldWorkDropDownCity() {
+//        $("[data-test-id=\"city\"] .input__control").setValue("горо");
+//
+//        $$(".menu-item .menu-item__control").first().shouldHave(Condition.text("Белгород"));
+//        $$(".menu-item .menu-item__control").filter(Condition.visible).first().click();
+//        $("[data-test-id=\"city\"] .input__control").shouldHave(Condition.value("Белгород"));
+//        $("[data-test-id=\"date\"] .input__control").doubleClick().sendKeys(planningDate);
+//        $("[data-test-id=\"name\"] .input__control").setValue("Пупкин Василий");
+//        $("[data-test-id=\"phone\"] .input__control").setValue("+79123456789");
+//        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+//        $(".form-field .button__content").click();
+//        $("[data-test-id=\"notification\"] .notification__title").should(Condition.visible, Duration.ofSeconds(15));
+//        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
+//    }
 
-        $$(".menu-item .menu-item__control").first().shouldHave(Condition.text("Белгород"));
-        $$(".menu-item .menu-item__control").filter(Condition.visible).first().click();
-        $("[data-test-id=\"city\"] .input__control").shouldHave(Condition.value("Белгород"));
-        $("[data-test-id=\"date\"] .input__control").doubleClick().sendKeys(planningDate);
-        $("[data-test-id=\"name\"] .input__control").setValue("Пупкин Василий");
-        $("[data-test-id=\"phone\"] .input__control").setValue("+79123456789");
-        $("[data-test-id=\"agreement\"] .checkbox__box").click();
-        $(".form-field .button__content").click();
-        $("[data-test-id=\"notification\"] .notification__title").should(Condition.visible, Duration.ofSeconds(15));
-        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
-    }
-
-    @Test
-    public void shouldWorkCalendarChoose() {
-
-        $("[data-test-id=\"city\"] .input__control").setValue("Петрозаводск");
-        $("[data-test-id=\"name\"] .input__control").setValue("Пупкин Василий");
-
-        $("[data-test-id=\"date\"] .input__control").click();
-        while (!($(parameterString).exists())){
-            $(".calendar__title .calendar__arrow_direction_right[data-step=\"1\"]").click();
-        }
-        $(parameterString).click();
-        $("[data-test-id=\"date\"] .input__control").shouldHave(Condition.value(secondPlanningDate));
-
-        $("[data-test-id=\"phone\"] .input__control").setValue("+79123456789");
-        $("[data-test-id=\"agreement\"] .checkbox__box").click();
-        $(".form-field .button__content").click();
-        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + secondPlanningDate), Duration.ofSeconds(15));
-
-    }
+//    @Test
+//    public void shouldWorkCalendarChoose() {
+//
+//        $("[data-test-id=\"city\"] .input__control").setValue("Петрозаводск");
+//        $("[data-test-id=\"name\"] .input__control").setValue("Пупкин Василий");
+//
+//        $("[data-test-id=\"date\"] .input__control").click();
+//        while (!($(parameterString).exists())){
+//            $(".calendar__title .calendar__arrow_direction_right[data-step=\"1\"]").click();
+//        }
+//        $(parameterString).click();
+//        $("[data-test-id=\"date\"] .input__control").shouldHave(Condition.value(secondPlanningDate));
+//
+//        $("[data-test-id=\"phone\"] .input__control").setValue("+79123456789");
+//        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+//        $(".form-field .button__content").click();
+//        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + secondPlanningDate), Duration.ofSeconds(15));
+//
+//    }
 }
