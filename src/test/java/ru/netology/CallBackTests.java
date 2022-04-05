f926aeb3d1ccfa8aeb76065e45082854d1d07107
zaksignu <zaksignu@gmail.com>
2022-04-05 09:46:15 +0300

    minor changes for tests

--- src/test/java/ru/netology/CallBackTests.java
+++ src/test/java/ru/netology/CallBackTests.java
@@ -16,6 +16,7 @@ public class CallBackTests {
     int newDate = 40;
     String secondPlanningDate = generateDate(newDate);
     String parameterString = GeneratetSearchString (newDate);
+    String planningDate = generateDate(3);
 
     String generateDate(int days) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
@@ -42,7 +43,6 @@ public class CallBackTests {
 
     @Test
     public void shouldWorkHappyPAth() {
-        String planningDate = generateDate(3);
         $("[data-test-id=\"city\"] .input__control").setValue("Петрозаводск");
         $("[data-test-id=\"date\"] .input__control").doubleClick().sendKeys(planningDate);
         $("[data-test-id=\"name\"] .input__control").setValue("Пупкин Василий");
@@ -60,6 +60,13 @@ public class CallBackTests {
         $$(".menu-item .menu-item__control").first().shouldHave(Condition.text("Белгород"));
         $$(".menu-item .menu-item__control").filter(Condition.visible).first().click();
         $("[data-test-id=\"city\"] .input__control").shouldHave(Condition.value("Белгород"));
+        $("[data-test-id=\"date\"] .input__control").doubleClick().sendKeys(planningDate);
+        $("[data-test-id=\"name\"] .input__control").setValue("Пупкин Василий");
+        $("[data-test-id=\"phone\"] .input__control").setValue("+79123456789");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".form-field .button__content").click();
+        $("[data-test-id=\"notification\"] .notification__title").should(Condition.visible, Duration.ofSeconds(15));
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
     }
 
     @Test
