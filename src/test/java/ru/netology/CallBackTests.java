d19f0f7168aebbe5c897ce61aa9c987da112ddf6
zaksignu <zaksignu@gmail.com>
2022-04-05 10:37:41 +0300

    Appveyor changing added test

--- src/test/java/ru/netology/CallBackTests.java
+++ src/test/java/ru/netology/CallBackTests.java
@@ -69,23 +69,23 @@ public class CallBackTests {
 //        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
 //    }
 
-//    @Test
-//    public void shouldWorkCalendarChoose() {
-//
-//        $("[data-test-id=\"city\"] .input__control").setValue("Петрозаводск");
-//        $("[data-test-id=\"name\"] .input__control").setValue("Пупкин Василий");
-//
-//        $("[data-test-id=\"date\"] .input__control").click();
-//        while (!($(parameterString).exists())){
-//            $(".calendar__title .calendar__arrow_direction_right[data-step=\"1\"]").click();
-//        }
-//        $(parameterString).click();
-//        $("[data-test-id=\"date\"] .input__control").shouldHave(Condition.value(secondPlanningDate));
-//
-//        $("[data-test-id=\"phone\"] .input__control").setValue("+79123456789");
-//        $("[data-test-id=\"agreement\"] .checkbox__box").click();
-//        $(".form-field .button__content").click();
-//        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + secondPlanningDate), Duration.ofSeconds(15));
-//
-//    }
+    @Test
+    public void shouldWorkCalendarChoose() {
+
+        $("[data-test-id=\"city\"] .input__control").setValue("Петрозаводск");
+        $("[data-test-id=\"name\"] .input__control").setValue("Пупкин Василий");
+
+        $("[data-test-id=\"date\"] .input__control").click();
+        while (!($(parameterString).exists())){
+            $(".calendar__title .calendar__arrow_direction_right[data-step=\"1\"]").click();
+        }
+        $(parameterString).click();
+        $("[data-test-id=\"date\"] .input__control").shouldHave(Condition.value(secondPlanningDate));
+
+        $("[data-test-id=\"phone\"] .input__control").setValue("+79123456789");
+        $("[data-test-id=\"agreement\"] .checkbox__box").click();
+        $(".form-field .button__content").click();
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + secondPlanningDate), Duration.ofSeconds(15));
+
+    }
 }
